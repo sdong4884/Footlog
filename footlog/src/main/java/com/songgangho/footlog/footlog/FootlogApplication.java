c72da080de3a627c27041d5124030fd0c3f20d0e
Gang-Ho, LEE <happy96115@gmail.com>
2022-01-20 20:49:13 +0900

    Add files via upload

---
+++ footlog/src/main/java/com/songgangho/footlog/footlog/FootlogApplication.java
@@ -0,0 +1,13 @@
+package com.songgangho.footlog.footlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootlogApplication.class, args);
+	}
+
+}
