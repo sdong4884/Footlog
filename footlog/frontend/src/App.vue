8842c48bdf4c6043b81892e64a8294c5eaf1f0ba
thd2wkd <sdong4884@gmail.com>
2022-02-26 15:23:56 +0900

    router setting

--- footlog/frontend/src/App.vue
+++ footlog/frontend/src/App.vue
@@ -1,12 +1,13 @@
 <template>
-  <div>Foot Log</div>
+  <div id="app">
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'App',
   data: () => ({
-    //
   }),
 };
 </script>
---
+++ footlog/frontend/src/views/Index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Foot Log</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
